72fd8a6b5563be5fc2a55900d9dc14d3fd06e4d0
AxelSarmiento18 <axel.a.sarmiento@gmail.com>
2018-05-14 12:23:17 +0100

    Splash Android

--- repositories.Android/MainActivity.cs
+++ repositories.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace repositories.Droid
 {
-    [Activity(Label = "repositories", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "repositories", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
